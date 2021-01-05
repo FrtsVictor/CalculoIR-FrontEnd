414a8e8ffc42e2614d0c93b79f48097411d84684
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-04 23:13:01 -0300

    Add GlobalStyles

---
+++ calculo-irpf/src/styles/global.jsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+*{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box
+    min-height: 100%;
+}
+
+html, body{
+    font-family: 'Roboto', sans-serif;
+    height: 100%;
+    margin: 0 auto;
+}
+
+body, input, button {
+    font: 16px;
+}
+
+button{
+    cursor: pointer;
+}
+
+`;
