050b0d26684d9985f4123268c1258ed8de2d2d7c
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-07 23:33:51 -0300

    Add MenuItems

--- calculo-irpf/src/pages/home/index.jsx
+++ calculo-irpf/src/pages/home/index.jsx
@@ -3,24 +3,60 @@
 import { React, useState } from 'react';
 import { Header } from '../../components/Header';
 import {
-  LeftMenu, MenuContent, Container, ListContent
+  LeftMenu, Container, MenuContent
 } from './styles';
 import { MenuList } from '../../components/List';
-import { Item1, Item2, Item3 } from '../../components/List/items';
+import {
+  Item1, Item2, Item3, Item4, Item5
+} from '../../components/List/items';
 
 export const Home = () => {
   const [menu1, setMenu1] = useState(false);
   const [menu2, setMenu2] = useState(false);
   const [menu3, setMenu3] = useState(false);
+  const [menu4, setMenu4] = useState(false);
+  const [menu5, setMenu5] = useState(false);
 
-  const menuView1 = () => {
-    setMenu1(!menu1);
-  };
-  const menuView2 = () => {
-    setMenu2(!menu2);
-  };
-  const menuView3 = () => {
-    setMenu3(!menu3);
+  const activeMenu = (menu) => {
+    if (menu === 1) {
+      setMenu1(true);
+      setMenu2(false);
+      setMenu3(false);
+      setMenu4(false);
+      setMenu5(false);
+    }
+
+    if (menu === 2) {
+      setMenu1(false);
+      setMenu2(true);
+      setMenu3(false);
+      setMenu4(false);
+      setMenu5(false);
+    }
+
+    if (menu === 3) {
+      setMenu1(false);
+      setMenu2(false);
+      setMenu3(true);
+      setMenu4(false);
+      setMenu5(false);
+    }
+
+    if (menu === 4) {
+      setMenu1(false);
+      setMenu2(false);
+      setMenu3(false);
+      setMenu4(true);
+      setMenu5(false);
+    }
+
+    if (menu === 5) {
+      setMenu1(false);
+      setMenu2(false);
+      setMenu3(false);
+      setMenu4(false);
+      setMenu5(true);
+    }
   };
 
   return (
@@ -31,30 +67,21 @@ export const Home = () => {
         <LeftMenu>
 
           <MenuList
-            menuView1={menuView1}
-            menuView2={menuView2}
-            menuView3={menuView3}
-
+            activeMenu={activeMenu}
           />
         </LeftMenu>
         <MenuContent>
-          {menu1 && (
-          <ListContent>
-            <Item1 />
-          </ListContent>
-          )}
 
-          {menu2 && (
-          <ListContent>
-            <Item2 />
-          </ListContent>
-          )}
+          {menu1 && (<Item1 />)}
 
-          {menu3 && (
-          <ListContent>
-            <Item3 />
-          </ListContent>
-          )}
+          {menu2 && (<Item2 />)}
+
+          {menu3 && (<Item3 />)}
+
+          {menu4 && (<Item4 />)}
+
+          {menu5 && (<Item5 />)}
+
         </MenuContent>
       </Container>
     </>
