85dc2725bbdc75c8c07b33c24517c561083c2dfa
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-07 23:32:50 -0300

    Add AliquotaTable & styles

---
+++ calculo-irpf/src/components/AliquotaTable/styles.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const TableContainer = styled.div`
+display: grid;
+min-width: 700px;
+grid-template-columns: 1fr 1fr 1fr;
+
+ `;
+
+export const TableHead = styled.div`
+display: flex;
+height: 35px;
+font-weight: 600;
+justify-content: center;
+align-items:center;
+color: #FFFFFF;
+background-color: #000000;
+`;
+
+export const TableRow = styled.div`
+display: flex;
+align-items: center;
+height: 35px;
+
+`;
