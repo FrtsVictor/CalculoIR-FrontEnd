908337894d21a7b0b77f2da101b3a2ce9c5000fa
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-18 00:46:23 -0300

    Fixed display

--- calculo-irpf/src/components/List/styles.jsx
+++ calculo-irpf/src/components/List/styles.jsx
@@ -11,7 +11,9 @@ export const useStyles = makeStyles((theme) => ({
 
 export const TableContainer = styled.div`
 display: flex;
+flex: 1;
 font-size: 10px;
+min-width: 80%;
 `;
 
 export const TextContainer = styled.div`
@@ -19,10 +21,27 @@ display: flex;
 flex-direction: column;
 align-items: center;
 margin: 0 5%;
+
+div h4{
+  display: flex;
+  align-items: center;
+  justify-content:center;
+}
+
 `;
 
 export const ChartContainer = styled.div`
 display: flex;
 height: 400px;
 width: 400px
+`;
+
+export const Container = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+
+  h4{
+    margin-bottom:20px;
+  }
 `;
