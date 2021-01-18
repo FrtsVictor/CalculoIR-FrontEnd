e1068f2aa5cc5b1f5981e541c11aa1b8012af26f
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-18 15:46:11 -0300

    Organized imports

--- calculo-irpf/src/routes/index.js
+++ calculo-irpf/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+// pages
 import { Home } from '../pages/home';
 import { Login } from '../pages/login';
 import { SignIn } from '../pages/signIn';
