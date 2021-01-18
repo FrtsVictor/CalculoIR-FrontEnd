6c0f45dc530ddeb59fcfd586519cb9173680a7de
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-18 15:45:03 -0300

    Organized imports & fixed input types

--- calculo-irpf/src/components/FormCalcSalLiq/index.js
+++ calculo-irpf/src/components/FormCalcSalLiq/index.js
@@ -1,12 +1,14 @@
 import { React, useState } from 'react';
-import TextField from '@material-ui/core/TextField';
-import { UseStyles, FormContainer, ButtonContainer } from './styles';
-import { ButtonCalc } from '../Buttons';
+// api e hooks
 import { apiIRPF, verifyApiErrors } from '../../services';
-import { AlertMessage } from '../AlertMessage';
 import { useUser } from '../core/UserProvider/useUser';
+import { AlertMessage } from '../AlertMessage';
+// styles
+import { ButtonCalc } from '../Buttons';
+import { UseStyles, FormContainer, ButtonContainer } from './styles';
+import TextField from '@material-ui/core/TextField';
 
-export const FormCalcSalLiq = ({ getUser }) => {
+export const FormCalcSalLiq = ({ getTableContent }) => {
   const classes = UseStyles();
   // user
   const { user } = useUser();
@@ -14,6 +16,7 @@ export const FormCalcSalLiq = ({ getUser }) => {
   const [grossSalary, setGrossSalary] = useState(user.salarioMensal || 0);
   const [dependents, setDependents] = useState(user.dependentes || 0);
   const [childSupport, setChildSupport] = useState(user.pensaoAlimenticia || 0);
+  const [discounts, setDiscounts] = useState(0);
   // errors
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -24,14 +27,16 @@ export const FormCalcSalLiq = ({ getUser }) => {
   };
 
   const calculate = async () => {
+
     const calculation = {
       nome: name,
       salarioMensalBruto: grossSalary,
       dependentes: dependents,
       pensaoAlimenticia: childSupport,
+      outrosDescontos: discounts
     };
 
-    await apiIRPF.calculte.IRRF(calculation)
+    await apiIRPF.calculte.SalLiq(calculation)
       .then((data) => {
         setLoading(true);
 
@@ -42,7 +47,7 @@ export const FormCalcSalLiq = ({ getUser }) => {
           return;
         }
 
-        getUser(data.nome ? data : null);
+        getTableContent(data.nome ? data : null);
         resetFiled();
       }).finally(
         () => setLoading(false),
@@ -64,27 +69,31 @@ export const FormCalcSalLiq = ({ getUser }) => {
             />
             <TextField
               id="grosSalary"
+              type="number"
               label="Salario Mensal Bruto"
               value={grossSalary}
               onChange={(e) => setGrossSalary(e.target.value)}
             />
             <TextField
               id="dependents"
+              type="number"
               label="Dependentes"
               value={dependents}
               onChange={(e) => setDependents(e.target.value)}
             />
             <TextField
               id="childSupport"
+              type="number"
               label="Pensao alimenticia"
               value={childSupport}
               onChange={(e) => setChildSupport(e.target.value)}
             />
             <TextField
               id="childSupport"
-              label="Total de descontos"
-              value={childSupport}
-              onChange={(e) => setChildSupport(e.target.value)}
+              type="number"
+              label="Outros descontos"
+              value={discounts}
+              onChange={(e) => setDiscounts(e.target.value)}
             />
           </div>
 
