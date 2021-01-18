09cc1a1c4eddd71e9109a75e66fdb6996323ac08
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-18 00:42:51 -0300

    Added Modal User

---
+++ calculo-irpf/src/components/ModalUser/index.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+// Icons & Buttons
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import { FormUser } from '../FormUser';
+// Api && Hooks
+import { useUser } from '../core/UserProvider/useUser';
+
+// Styles
+import {
+  PapperContainer, Container
+
+} from './styles';
+
+export const ModalImovel = ({ handleClose, open }) => {
+  const { user: { id } } = useUser();
+
+  console.log(id);
+
+  return (
+    <Container>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+
+          <PapperContainer>
+
+            <IconButton
+              style={{
+                display: 'flex',
+                alignItems: 'flex-end',
+                marginBottom: '90%',
+                maxHeight: '15px',
+                maxWidth: '15px',
+                color: '#eb987f',
+              }}
+              type="button"
+              onClick={() => handleClose()}
+            >
+              <CloseIcon />
+            </IconButton>
+
+            <FormUser />
+
+          </PapperContainer>
+        </Fade>
+      </Modal>
+    </Container>
+  );
+};
---
+++ calculo-irpf/src/components/ModalUser/styles.jsx
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+// Containers
+
+export const Container = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+export const PapperContainer = styled.div`
+    display: flex;
+    width: 30vw;
+    height: 70vh;
+    border-radius: 20px;
+    flex-wrap: wrap;
+    margin:  10% auto;
+    color:#03020c;
+    background-color: #fff;
+    justify-content: center;
+    align-items:center;
+    transition: 500ms;
+    border-right:1px solid black;
+    font-style:Roboto, sans-serif;
+
+`;
+
+export const MenuText = styled.p`
+align-items:flex-start;
+justify-content: flex-start;
+display:flex;
+flex:1;
+`;
+
+export const InfoP = styled.p`
+font-size: 1rem;
+justify-content: center;
+display:flex;
+width:10%;
+`;
+
+export const IconInfo = styled.div`
+display:flex;
+flex-direction:column;
+align-items: center;
+width: 100%;
+margin-bottom: 3vh;
+`;
