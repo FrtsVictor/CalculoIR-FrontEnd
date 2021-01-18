932b7ea6bd45f9a23bdcda416e7e961847db27dc
FrtsVictor <victorfreitas.job@gmail.com>
2021-01-18 15:47:46 -0300

    Added SalLiqTable

---
+++ calculo-irpf/src/components/SalLiqTable/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+  TableContainer, TableRow, TableHead
+} from './styles';
+
+export const SalLiqTable = ({ tableContent }) => {
+  const currencyFormater = (currencyValue) => new Intl.NumberFormat(
+    'pt-BR', { style: 'currency', currency: 'BRL' }
+  ).format(currencyValue);
+
+  return (
+    <TableContainer>
+      <TableHead>
+        <p>{tableContent.nome}</p>
+      </TableHead>
+
+      <TableRow>
+        <p>Salario bruto</p>
+        <p>{currencyFormater(tableContent.salarioMensalBruto)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>INSS</p>
+        <p>{currencyFormater(tableContent.inss)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Qtd dependentes</p>
+        <p>{tableContent.dependentes}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Valor dependentes</p>
+        <p>{currencyFormater(tableContent.valorDependentes)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Pensao alimenticia</p>
+        <p>{currencyFormater(tableContent.pensaoAlimenticia)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Outros descontos</p>
+        <p>{currencyFormater(tableContent.outrosDescontos)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Valor total descontos</p>
+        <p>{currencyFormater(tableContent.valorTotalDescontos)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Base Calculo</p>
+        <p>{currencyFormater(tableContent.baseCalculo)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Aliquota IRRF</p>
+        <p>{`${tableContent.porcentagemAliquota}%`}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Parcela a deduzir</p>
+        <p>{currencyFormater(tableContent.parcelaADeduzir)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>IRRF(Imposto retido na fonte)</p>
+        <p>{currencyFormater(tableContent.irrf)}</p>
+      </TableRow>
+
+      <TableRow>
+        <p>Salario liquido</p>
+        <p>{currencyFormater(tableContent.salarioLiquido)}</p>
+      </TableRow>
+
+    </TableContainer>
+  );
+};
---
+++ calculo-irpf/src/components/SalLiqTable/styles.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const TableContainer = styled.div`
+display: flex;
+min-width: 300px;
+flex-direction: column;
+font-size: 0.90rem;
+
+div:nth-child(2n+2) {
+    background: #F0F0F0;
+}
+
+`;
+
+export const TableRow = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+height: 35px;
+
+p{
+  margin-right: 10px;
+}
+
+`;
+
+export const TableHead = styled.div`
+display: flex;
+height: 35px;
+align-items: center;
+font-weight: 600;
+justify-content: center;
+color: #FFFFFF;
+background-color: #000000;
+`;
